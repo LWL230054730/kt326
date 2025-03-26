d8c3837cee66a5b5e71da10e28474bc5f51da0bd
alex <230054730@stu.vtc.edu.hk>
2025-03-27 01:49:22 +0800

    pose picture

--- app/src/main/java/com/example/fyp/AnalysisActivity.java
+++ app/src/main/java/com/example/fyp/AnalysisActivity.java
@@ -5,29 +5,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class AnalysisActivity extends Fragment {
-    private Button startCaptureButton;
+    private ImageView gridImage1;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // 加載佈局檔案
         View view = inflater.inflate(R.layout.activity_analysis, container, false);
 
-        // 初始化按鈕
-        startCaptureButton = view.findViewById(R.id.start_capture_button);
+        gridImage1 = view.findViewById(R.id.grid_image_1);
 
-        // 設置按鈕點擊事件
-        startCaptureButton.setOnClickListener(new View.OnClickListener() {
+        gridImage1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 跳轉到 PoseDetectionActivity
                 Intent intent = new Intent(getActivity(), PoseDetectionActivity.class);
                 startActivity(intent);
             }
